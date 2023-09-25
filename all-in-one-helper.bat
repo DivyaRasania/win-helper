cda70487ade7d23a9aac6914756b9e28fd7199a5
Divya Rasania <70239770+DivyaRasania@users.noreply.github.com>
2023-09-25 14:54:29 -0400

    Update all-in-one-helper.bat

--- all-in-one-helper.bat
+++ all-in-one-helper.bat
@@ -4,27 +4,32 @@ REM creates a battery report on desktop
 powercfg /batteryreport /output "%userprofile%\Desktop\battery-report.html"
 
 REM opens battery report
-%userprofile%\Desktop\battery-report.html
+start msedge "%userprofile%\Desktop\battery-report.html"
 
 REM repair file or system image corruptions
+echo "Starting system repairs"
 sfc /scannow
 DISM /Online /Cleanup-Image /RestoreHealth
 
 REM updates all softwares
+echo "Starting software updates"
 winget upgrade --include-unknown --all --accept-package-agreements --accept-source-agreements --force --silent --disable-interactivity
 
 REM opens disk cleaner
+echo "Starting disk cleaner"
 cleanmgr
 
-REM deleting all temporary files
+REM deleting all temporary files & battery-report
+echo "Removing temp files"
 del /s /q "C:\Users\rasan\AppData\Local\Temp\*"
 del /s /q "C:\Windows\Temp\*"
 del /s /q "C:\Windows\Prefetch\*"
 del %userprofile%\Desktop\battery-report.html
 
-REM refreshing Wi-Fi
+REM refreshing networks
+echo "Refreshing networks"
 netsh winsock reset
-ipconfig /release "Wi-Fi"
+ipconfig /release
 ipconfig /renew
 ipconfig /flushdns
 
