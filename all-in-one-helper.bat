7126f268add241148c01a963d0058ba288e1c205
Divya Rasania <70239770+DivyaRasania@users.noreply.github.com>
2023-10-02 16:17:54 -0400

    added some echos for user and signed my file

--- all-in-one-helper.bat
+++ all-in-one-helper.bat
@@ -1,37 +1,42 @@
 @echo off
 
+REM created by Divya Rasania
+
 REM creates a battery report on desktop
+echo "==========Outputing battery report=========="
 powercfg /batteryreport /output "%userprofile%\Desktop\battery-report.html"
 
 REM opens battery report
+echo "==========Opening battery report=========="
 start msedge "%userprofile%\Desktop\battery-report.html"
 
 REM repair file or system image corruptions
-echo "Starting system repairs"
+echo "==========Starting system repairs=========="
 sfc /scannow
 DISM /Online /Cleanup-Image /RestoreHealth
 
 REM updates all softwares
-echo "Starting software updates"
+echo "==========Starting software updates=========="
 winget upgrade --include-unknown --all --accept-package-agreements --accept-source-agreements --force --silent --disable-interactivity
 
 REM opens disk cleaner
-echo "Starting disk cleaner"
+echo "==========Starting disk cleaner=========="
 cleanmgr
 
 REM deleting all temporary files & battery-report
-echo "Removing temp files"
+echo "==========Removing temp files=========="
 del /s /q "C:\Users\rasan\AppData\Local\Temp\*"
 del /s /q "C:\Windows\Temp\*"
 del /s /q "C:\Windows\Prefetch\*"
 del %userprofile%\Desktop\battery-report.html
 
 REM refreshing networks
-echo "Refreshing networks"
+echo "==========Refreshing networks=========="
 netsh winsock reset
 ipconfig /release
 ipconfig /renew
 ipconfig /flushdns
 
 REM restarts the computer for fresh start
+echo "==========Restaring your pc=========="
 shutdown /r /t 30 /c "Your computer will restart in 30 seconds. Please save all your files in that time."
