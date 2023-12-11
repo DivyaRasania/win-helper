24b68ff6547c5692215ba7a1bdf64a4364d34c3b
Divya Rasania <70239770+DivyaRasania@users.noreply.github.com>
2023-12-11 11:49:02 -0500

    Update all-in-one-helper.bat

--- all-in-one-helper.bat
+++ all-in-one-helper.bat
@@ -1,42 +1,43 @@
 @echo off
 
-REM created by Divya Rasania
+REM Script created by Divya Rasania
+REM This script performs various system maintenance tasks and restarts the computer.
 
-REM creates a battery report on desktop
-echo "==========Outputing battery report=========="
+REM Output battery report to desktop
+echo "========== Outputing battery report =========="
 powercfg /batteryreport /output "%userprofile%\Desktop\battery-report.html"
 
-REM opens battery report
-echo "==========Opening battery report=========="
+REM Open battery report
+echo "========== Opening battery report =========="
 start msedge "%userprofile%\Desktop\battery-report.html"
 
-REM repair file or system image corruptions
-echo "==========Starting system repairs=========="
+REM Repair file or system image corruptions
+echo "========== Starting system repairs =========="
 sfc /scannow
 DISM /Online /Cleanup-Image /RestoreHealth
 
-REM updates all softwares
-echo "==========Starting software updates=========="
-winget upgrade --include-unknown --all --accept-package-agreements --accept-source-agreements --force --silent
+REM Update all softwares
+echo "========== Starting software updates =========="
+winget upgrade --all
 
-REM opens disk cleaner
-echo "==========Starting disk cleaner=========="
+REM Open Disk Cleanup
+echo "========== Starting Disk Cleanup =========="
 cleanmgr
 
-REM deleting all temporary files & battery-report
-echo "==========Removing temp files=========="
-del /s /q "C:\Users\rasan\AppData\Local\Temp\*"
+REM Delete temporary files and battery report
+echo "========== Removing temp files =========="
+del /s /q "%localappdata%\Temp\*"
 del /s /q "C:\Windows\Temp\*"
 del /s /q "C:\Windows\Prefetch\*"
 del %userprofile%\Desktop\battery-report.html
 
-REM refreshing networks
-echo "==========Refreshing networks=========="
+REM Refresh network settings
+echo "========== Refreshing networks =========="
 netsh winsock reset
 ipconfig /release
 ipconfig /renew
 ipconfig /flushdns
 
-REM shutsdown the computer for fresh start
-echo "==========Shutting down your pc=========="
-SlideToShutdown
+REM Restart computer with message
+echo "========== Restarting your PC... =========="
+shutdown -r -t 30 -c "Your PC is about to restart in 30 seconds. Please save your work."
